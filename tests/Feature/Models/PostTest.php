f5d916a76bb29e67e2cd293df1c0e14740786d4b
Amir Tavakolian <rezatva@gmail.com>
2025-06-22 17:46:22 +0330

    test post belongs to user

--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -23,4 +23,24 @@ class PostTest extends TestCase
 
         $this->assertInstanceOf(User::class, $post->user);
     }
+
+    public function test_post_belongs_to_user1()
+    {
+        $user = User::factory()->create();
+
+        $post = Post::factory()->state([
+            'user_id' => $user->id
+        ])->create();
+
+        $this->assertEquals($user->id, $post->id);
+    }
+
+    public function test_post_belongs_to_user2()
+    {
+        $user = User::factory()->create();
+
+        $post = Post::factory()->for($user)->create();
+
+        $this->assertEquals($user->id, $post->id);
+    }
 }
